6cbcaba4518b438d1497c544b4dfd936df72eac1
mateusz83 <32179429+mateusz83@users.noreply.github.com>
2022-08-26 16:33:23 +0200

    Add files via upload

---
+++ msRay_devpack_v0.33/EngineCommonLibs/GR_Game_Render.h
@@ -0,0 +1,14 @@
+#ifndef __GR_GAME_RENDER_H__
+#define __GR_GAME_RENDER_H__
+
+#include "IO_In_Out.h"
+
+// ---------------------------------------------------
+// --- GAME RENDER - PUBLIC functions declarations ---
+// ---------------------------------------------------
+int32	GR_Init(void);
+void	GR_Reset(void);
+void	GR_Run(void);
+void	GR_Cleanup(void);
+
+#endif
---
+++ msRay_devpack_v0.33/EngineCommonLibs/PL_Player.h
@@ -0,0 +1,44 @@
+#ifndef __PL_PLAYER_H__
+#define __PL_PLAYER_H__
+
+#include "IO_In_Out.h"
+
+// -----------------------------------------------------------
+// --- GAME OBJECTS - PUBLIC globals, constants, variables ---
+// -----------------------------------------------------------
+
+// Player attributes.
+#define PL_MOVE_SPEED        2.0f     
+#define PL_Z_SPEED           10   
+#define PL_Z_MAX             10.0f
+#define PL_Z_LUT_SIZE        256
+#define PL_Z_LUT_SIZE__SUB1  255
+
+// Player structure and extern declaration.
+typedef struct
+{
+	// Precalculated z position array, used while moving.
+	float32 z__LUT[PL_Z_LUT_SIZE];
+
+	// Player orientation parameters.
+	float32 x, y, z;
+	float32 angle_radians, pitch;
+	float32 z_accumulation;
+	float32 pitch_speed;
+
+	float32 pitch_max;
+
+	int32	curr_cell_id;
+	int8	curr_cell_x, curr_cell_y;
+
+}_IO_BYTE_ALIGN_ sPL_Player;
+
+sPL_Player PL_player;
+
+// ----------------------------------------------------
+// --- GAME OBJECTS - PUBLIC functions declarations ---
+// ----------------------------------------------------
+void PL_Player_Init(void);
+void PL_Player_Reset(void);
+
+#endif
